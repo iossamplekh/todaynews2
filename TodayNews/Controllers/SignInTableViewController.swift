c1a5f39764a6e11f2e2f57e5f87930a6b84d4386
rithlkh2 <rithronlkh@gmail.com>
2018-02-07 22:16:59 +0700

    add signin ui

---
+++ TodayNews/Controllers/SignInTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignInTableViewController.swift
+//  TodayNews
+//
+//  Created by Ron Rith on 2/7/18.
+//  Copyright © 2018 Ron Rith. All rights reserved.
+//
+
+import UIKit
+
+class SignInTableViewController: UITableViewController{
+    @IBOutlet var userEmailTextField: UITextField!
+    @IBOutlet var userPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
