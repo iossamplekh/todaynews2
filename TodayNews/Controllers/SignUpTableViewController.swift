0c2cbb1c7d196479799ce076a2125b2dc8a57024
rithlkh2 <rithronlkh@gmail.com>
2018-02-13 22:23:19 +0700

    add browser photo and that photo in image view after choose

--- TodayNews/Controllers/SignUpTableViewController.swift
+++ TodayNews/Controllers/SignUpTableViewController.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 import SCLAlertView
+import SwiftyJSON
+import NVActivityIndicatorView
 
-class SignUpTableViewController: UITableViewController {
+class SignUpTableViewController: UITableViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate, NVActivityIndicatorViewable {
+    
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var lastNameTextField: UITextField!
     @IBOutlet var genderSegmentedControl: UISegmentedControl!
     
+    let imagePicker = UIImagePickerController()
+    
     @IBOutlet var userImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        imagePicker.delegate = self
     }
     @IBAction func signUp(_ sender: Any) {
         let parameters =
@@ -29,10 +34,34 @@ class SignUpTableViewController: UITableViewController {
             "name": nameTextField.text ?? "",
             "lastName": lastNameTextField.text ?? ""
         ]
+        
+        // image
+        let photo = ["file": UIImageJPEGRepresentation(self.userImageView.image!, 1)!]
+        
         UserService.shared.singup(paramaters: parameters) { (response, error) in
             
         }
     }
     
+    @IBAction func browserPhotoTapGesture(_ sender: Any) {
+        print(#function)
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+        
+        if let pickedImage = info[UIImagePickerControllerEditedImage] as? UIImage{
+            self.userImageView.contentMode = .scaleAspectFit
+            self.userImageView.image = pickedImage
+            print("pickedImage: \(pickedImage)")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
     
 }
