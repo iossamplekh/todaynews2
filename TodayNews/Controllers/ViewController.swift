6627684af03938d0f55a871505ab98d47bfac598
rithlkh2 <rithronlkh@gmail.com>
2018-03-07 20:13:20 +0700

    working

--- TodayNews/Controllers/ViewController.swift
+++ TodayNews/Controllers/ViewController.swift
@@ -254,10 +254,8 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     
     @IBAction func logout(_ sender: Any) {
-<<<<<<< HEAD
+
         print("USER ID: \(UserDefaults.standard.string(forKey: "userID"))")
-=======
->>>>>>> a9562d8c80e10968ed3c4c3d7648f39fa1f9b47a
         UserDefaults.standard.removeObject(forKey: "UserID")
         self.dismiss(animated: true, completion: nil)
     }
